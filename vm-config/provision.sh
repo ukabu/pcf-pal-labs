b86eb24da452e5514d32a8b6404b5a20fcdb5bf0
Ukabu <emmanuel@pirsch.org>
2017-03-14 02:25:06 -0400

    Provision RabbitMQ

--- vm-config/provision.sh
+++ vm-config/provision.sh
@@ -9,6 +9,9 @@ echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/so
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
+# configure RabbitMQ debian repo
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
 
 # add ppa for Java builds - to get java 8 on trusty
 add-apt-repository -y ppa:openjdk-r/ppa
@@ -16,7 +19,7 @@ add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 
 # Install cf client, curl, git, jq (json parser/prettyfier), siege (HTTP load tester)
-apt-get install -y cf-cli curl git grc jq openjdk-8-jdk mongodb-org siege unzip zip
+apt-get install -y cf-cli curl git grc jq openjdk-8-jdk mongodb-org rabbitmq-server siege unzip zip
 
 # install mysql server without root password
 apt-get -y install mysql-server
